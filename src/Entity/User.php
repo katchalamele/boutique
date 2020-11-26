60fc106e6f0700da105482fad40b18defad0e08c
Daouda KATCHALA MELE <davidkatchala@gmail.com>
2020-11-26 06:05:36 +0100

    correction du bug de user pour pgsql

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
  */
 class User implements UserInterface
 {
